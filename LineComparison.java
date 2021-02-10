f7d68152c7ca58459ca9a395354d2226653fdaa2
Dipesh Maywade <dipeshmaywade@gmail.com>
2021-02-10 20:13:09 +0530

    [Deep]Updated: file updated for calculating Distance b/w points

--- LineComparison.java
+++ LineComparison.java
@@ -1,6 +1,24 @@
+import java.util.Scanner;
+
 public class LineCompariosn{
 	public static void main(String[] args){
-		System.out.println("Welcome To Line Comparison Computation Program");
+
+		Scanner a = new Scanner(System.in);
+		System.out.println("Enter the Value Of X1");
+		int x1 = a.nextInt();
+
+                System.out.println("Enter the Value Of y1");
+                int y1 = a.nextInt();
+
+                System.out.println("Enter the Value Of x2");
+                int x2 = a.nextInt();
+
+                System.out.println("Enter the Value Of y2");
+                int y2 = a.nextInt();
+
+		double lengthOfLine= Math.sqrt((x2-x1)^2+(y2-y1)^2);
+		System.out.println("DistanceBetween"+"("+x1+","+y1+"),"+"("+x2+","+y2+")===>"+lengthOfLine);
+
 	}
 
 }
