6c03980700f813ecbf39cc6768ba9573009e6b44
Dipesh Maywade <dipeshmaywade@gmail.com>
2021-02-12 21:55:48 +0530

    [Deep]Update file for Compairing 2 line length

--- LineComparison.java
+++ LineComparison.java
@@ -1,48 +1,42 @@
 import java.util.Scanner;
 
-public class LineCompariosn{
-	public static void main(String[] args){
+public class LineProblem {
 
-		Scanner a = new Scanner(System.in);
-		System.out.println("Enter the Value Of X1");
-		int l1x1 = a.nextInt();
+    public static void main(String[] args) {
 
-                System.out.println("Enter the Value Of y1");
-                int l1y1 = a.nextInt();
+        System.out.println("Enter Coordinates For Line1");
+        double lengthOfLine1= lengthCalculation();
+        System.out.println("Enter Coordinates For Line2");
+        double lengthOfLine2= lengthCalculation();
 
-                System.out.println("Enter the Value Of x2");
-                int l1x2 = a.nextInt();
+        System.out.println("Length Of Line1 "+ lengthOfLine1);
+        System.out.println("Length Of Line2 "+ lengthOfLine2);
 
-                System.out.println("Enter the Value Of y2");
-                int l1y2 = a.nextInt();
+        if(lengthOfLine1==lengthOfLine2){
+            System.out.println("Both Line Are Equal");
+        }
+        else if (lengthOfLine1>lengthOfLine2){
+            System.out.println("Line1 is Bigger Than Line2");
+        }
+        else if (lengthOfLine1<lengthOfLine2){
+            System.out.println("Line2 is Bigger Than Line1" );
+        }
+    }
 
-		double lengthOfLine1= Math.sqrt((l1x2-l1x1)^2+(l1y2-l1y1)^2);
-		System.out.println("DistanceBetween"+"("+l1x1+","+l1y1+"),"+"("+l1x2+","+l1y2+")===>"+lengthOfLine1);
+    static double lengthCalculation(){
+        double x1,x2,y1,y2;
+        double dis;
 
+        Scanner sc=new Scanner(System.in);
 
-		System.out.println("Enter the Value Of X1");
-                int l2x1 = a.nextInt();
+        System.out.println("enter x1,y1,x2,y2 point");
+        x1=sc.nextDouble();
+        y1=sc.nextDouble();
+        x2=sc.nextDouble();
+        y2=sc.nextDouble();
 
-                System.out.println("Enter the Value Of y1");
-                int l2y1 = a.nextInt();
+        dis= Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
 
-                System.out.println("Enter the Value Of x2");
-                int l2x2 = a.nextInt();
-
-                System.out.println("Enter the Value Of y2");
-                int l2y2 = a.nextInt();
-
-                double lengthOfLine2= Math.sqrt((l2x2-l2x1)^2+(l2y2-l2y1)^2);
-                System.out.println("DistanceBetween"+"("+l2x1+","+l2y1+"),"+"("+l2x2+","+l2y2+")===>"+lengthOfLine2);
-
-		if (lengthOfLine1==lengthOfLine2){
-
-			System.out.println("Both Line are Equal");
-		}
-
-		else {
-			System.out.println("Line Are Not Equal");
-		}
-	}
-
+        return dis;
+    }
 }
