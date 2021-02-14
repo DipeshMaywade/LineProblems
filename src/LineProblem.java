f755eeaee16ee5164bf38970ef7f4bcd6f73ef5b
Dipesh Maywade <dipeshmaywade@gmail.com>
2021-02-14 21:04:01 +0530

    [Deep]Update: File change for use constructor(OOP)

--- src/LineProblem.java
+++ src/LineProblem.java
@@ -1,28 +1,25 @@
 import java.util.Scanner;
 
 public class LineProblem {
-    private final double x1;
-    private final double y1;
-    private final double x2;
-    private final double y2;
+    private final double X1;
+    private final double Y1;
+    private final double X2;
+    private final double Y2;
 
     public LineProblem() {
         Scanner sc = new Scanner(System.in);
-        this.x1 = sc.nextDouble();
-        this.y1 = sc.nextDouble();
-        this.x2 = sc.nextDouble();
-        this.y2 = sc.nextDouble();
+        this.X1 = sc.nextDouble();
+        this.Y1 = sc.nextDouble();
+        this.X2= sc.nextDouble();
+        this.Y2 = sc.nextDouble();
     }
 
-    public static void calculation() {
+    public static void main(String[] args) {
 
-        System.out.println("For Line One");
-        LineProblem line1 = new LineProblem();
-        double lengthOfLine1 = Math.sqrt((line1.x2 - line1.x1) * (line1.x2 - line1.x1) + (line1.y2 - line1.y1) * (line1.y2 - line1.y1));
-        System.out.println("For Line Two");
-        LineProblem line2 = new LineProblem();
-        double lengthOfLine2 = Math.sqrt((line2.x2 - line2.x1) * (line2.x2 - line1.x1) + (line2.y2 - line2.y1) * (line2.y2 - line2.y1));
-
+        System.out.println("Enter Coordinates For Line1 x1,y1,x2,y2");
+        double lengthOfLine1 = calculation();
+        System.out.println("Enter Coordinates For Line2 x1,y1,x2,y2");
+        double lengthOfLine2 = calculation();
 
         System.out.println("Length Of Line1 " + lengthOfLine1);
         System.out.println("Length Of Line2 " + lengthOfLine2);
@@ -35,8 +32,10 @@ public class LineProblem {
             System.out.println("Line2 is Bigger Than Line1");
         }
     }
-    public static void main(String[] args) {
-        calculation();
-    }
 
+    public static double calculation() {
+        LineProblem obj = new LineProblem();
+        double length = Math.sqrt(Math.pow((obj.X2 - obj.X1),2) + Math.pow((obj.Y2 - obj.Y1),2));
+        return length;
+    }
 }
